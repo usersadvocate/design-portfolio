e8d2281989bdaf5823824dfec5572ae0ebb26d2d
Julia Pchelova <kozeeva.yulya@gmail.com>
2025-07-04 21:48:18 +0200

    test: check visible output

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import "./styles/global.scss";
 import AppRoutes from "./routes/AppRoutes";
 
+// export default function App() {
+//   return <AppRoutes />;
+// }
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <div style={{ padding: "2rem", color: "black" }}>
+      Hello from App! This is a test.
+    </div>
+  );
 }
