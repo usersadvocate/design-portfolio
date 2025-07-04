c0dd330807ff6290f9ca1b53c0247296f9f40b2e
Julia Pchelova <kozeeva.yulya@gmail.com>
2025-07-04 22:49:30 +0200

    Fix images path for Vercel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,21 +15,21 @@ export default function Home() {
         description="Creating a procurement tool on a cloud platform from scratch"
         years="2023"
         scope="Product Market Fit"
-        imageSrc="/images/BOM.png"
+        imageSrc="./images/BOM.png"
       />
       <ProjectSection
         title="Continuous Discovery"
         description="Framework on how to continuously validate and improve products to meet customer needs"
         years="2022"
         scope="User Research"
-        imageSrc="/images/Discovery.png"
+        imageSrc="./images/Discovery.png"
       />
       <ProjectSection
         title="Plego"
         description="Team work on scalability of the design system"
         years="2022"
         scope="Design system"
-        imageSrc="/images/Plego.png"
+        imageSrc="./images/Plego.png"
       />
       <Footer />
     </div>
