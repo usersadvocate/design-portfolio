105006e30c777ff3f62ca69e3484e1ff135a720f
Julia Pchelova <kozeeva.yulya@gmail.com>
2025-07-07 19:08:51 +0200

    Add CopyWebpackPlugin to copy image assets to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -36,6 +37,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/images"),
+          to: "images",
+        },
+      ],
     }),
   ],
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
